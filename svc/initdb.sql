1b47f4c2f564bcb5ce47eb40a2be4d5cf44c7e21
Edward LEI <charming.lei72@qq.com>
2021-01-04 22:07:09 +0800

    add more comments

--- svc/initdb.sql
+++ svc/initdb.sql
@@ -2,10 +2,12 @@
 -- create a super user for this demo
 [postgres@xxxxxx ~]$ createuser --interactive
 
-
+-- create SCHEMA
 CREATE SCHEMA identity;
+-- set search_path
 SET search_path=identity;
 
+-- create table users (for demo purpose)
 CREATE TABLE users(
   staff_id SERIAL PRIMARY KEY,
   first_name VARCHAR(16) NOT NULL,
@@ -14,6 +16,7 @@ CREATE TABLE users(
   age INTEGER
 );
 
+-- some data to make table users dirty
 INSERT INTO users VALUES(DEFAULT, 'Bill', 'Gates', 'bg@microsoft.com', 62);
 INSERT INTO users VALUES(DEFAULT, 'Michael', 'Jackson', 'mj@google.com', 68);
 INSERT INTO users VALUES(DEFAULT, 'Marry', 'Popins', 'mp@magicworld.com', 208);
